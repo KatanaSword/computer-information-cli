b0a3aa70f4110612edd0aa01788dea99b2d7e66d
Saurabh Tajane <114555620+KatanaSword@users.noreply.github.com>
2024-06-12 16:29:39 +0530

    Added memory info feature

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import platform
 import psutil
 
+def get_size(bytes, suffix="B"):
+    """
+    Scaling bytes to its proper format- KB, MB, GB, TB and PB 
+    """
+    the_factor = 1024
+    for the_unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < the_factor:
+            return f"{bytes:.2f} {the_unit}{suffix}"
+        bytes /= the_factor
+
 def system_info():
     sys  = platform.uname()
     print(f"\n{"*"*30} System Information {"*"*30}")
@@ -19,6 +29,21 @@ def system_info():
     print(f"Min frequency: {cup_freq.min:.2f}Mhz")
     print(f"Current frequency: {cup_freq.current:.2f}Mhz")
 
+def memory_info():
+    vm = psutil.virtual_memory()
+    print(f"\n{"*"*30} Virtual Memory {"*"*30}")
+    print(f"Total: {get_size(vm.total)}")
+    print(f"Available: {get_size(vm.free)}")
+    print(f"Used: {get_size(vm.used)}")
+    print(f"Percentage: {vm.percent} %")
+
+    swap = psutil.swap_memory()
+    print(f"\n{"*"*30} SWAP Memory {"*"*30}")
+    print(f"Total: {get_size(swap.total)}")
+    print(f"Free: {get_size(swap.free)}")
+    print(f"Used: {get_size(swap.used)}")
+    print(f"Percentage: {swap.percent} %")
+
 def main():
     while True:
         print("\n1. Know Computer System.")
@@ -26,8 +51,9 @@ def main():
         print("3. Know Computer Network.")
         print("4. Know Computer GPU.")
         print("5. Know About Virtual Memory.")
+        print("6. Know About Storage.")
         print("0. Exit App")
-        choose = input("Enter a number to learn more about computer: ")
+        choose = input("\nEnter a number to learn more about computer: ")
 
         if choose == "1":
             system_info()
@@ -38,7 +64,9 @@ def main():
         elif choose == "4":
             gpu_info()
         elif choose == "5":
-            virtual_memory_info()
+            memory_info()
+        elif choose == "6":
+            storage_info()
         elif choose == "0":
             break
         else:
